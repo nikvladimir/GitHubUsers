e797c06f72c3c7b175d2c93c1d73e87eb501c650
Vladimir <alfa.voland@mail.ru>
2024-04-02 14:25:15 +0500

    added blanck fragment and viewBinding

--- app/src/main/java/com/alfadroid/githubusers/MainActivity.kt
+++ app/src/main/java/com/alfadroid/githubusers/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.alfadroid.githubusers
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.alfadroid.githubusers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragment = GitHubUsersFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(binding.fragmentContainer.id, fragment)
+            commit()
         }
+
     }
 }
