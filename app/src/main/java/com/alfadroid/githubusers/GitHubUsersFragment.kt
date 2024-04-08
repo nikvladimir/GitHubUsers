cc2b4ca08da2e9a1ec8667818a0daf7a7690c1a0
Vladimir <alfa.voland@mail.ru>
2024-04-08 12:36:48 +0500

    OkHttp request

--- app/src/main/java/com/alfadroid/githubusers/GitHubUsersFragment.kt
+++ app/src/main/java/com/alfadroid/githubusers/GitHubUsersFragment.kt
@@ -1,11 +1,17 @@
 package com.alfadroid.githubusers
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.alfadroid.githubusers.databinding.FragmentGitHubUsersBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 
 class GitHubUsersFragment : Fragment() {
@@ -21,6 +27,22 @@ class GitHubUsersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binging = FragmentGitHubUsersBinding.inflate(layoutInflater)
+
+        val URL = "https://api.github.com/users"
+        val okHttpClient = OkHttpClient()
+        val request = Request.Builder().url(URL).build()
+        val callback = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                println("Запрос не выполнился")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                println("Запрос выполнился УСПЕШНО ${response.body?.string()}")
+            }
+
+        }
+
+        okHttpClient.newCall(request).enqueue(callback)
 
         return binging.root
     }
